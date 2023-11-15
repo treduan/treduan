99d89b088c23fdb0ce0d46b12774fefb26b3c388
Anna Metsäpelto <anna.metsapelto1@tampere.fi>
2023-11-15 14:01:13 +0200

    Add content to lottery

--- php/testikansio/index.php
+++ php/testikansio/index.php
@@ -6,10 +6,24 @@
     <title>Document</title>
 </head>
 <body>
+    <p>
     <?php
     $name = "Anna";
     echo "Hello, $name!";
-    ?>
-    
+    ?></p>
+
+    <?php
+    $price = 550;
+    $income = 2500;
+    function expenses($income, $price) {
+        $percent = $price/$income;
+        if($percent >= 0.3)
+            return " that is quite costly.";
+        else 
+            return " you can afford that.";
+    }
+?>
+<p>If your income is <?php echo $income . " and you buy an item with $price, " . expenses($price, $income) . "." ?></p> 
+ 
 </body>
 </html>
