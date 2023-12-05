fd3a4fb2ee93b6cfdf396de3b9c4733423430ea1
Anna Metsäpelto <anna.metsapelto1@tampere.fi>
2023-12-05 15:02:36 +0200

    Make test

--- php/testikansio/index.php
+++ php/testikansio/index.php
@@ -24,6 +24,6 @@
     }
 ?>
 <p>If your income is <?php echo $income . " and you buy an item with $price, " . expenses($price, $income) . "." ?></p> 
- 
+<?php require "article.php" ?> 
 </body>
 </html>
