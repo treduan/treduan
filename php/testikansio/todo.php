2af79455a4486e11b5595491f4603813f7dd7d77
Anna Metsäpelto <anna.metsapelto1@tampere.fi>
2024-01-25 16:37:34 +0200

    Testing with sessions

--- php/testikansio/todo.php
+++ php/testikansio/todo.php
@@ -1,5 +1,4 @@
 <?php
- session_start();
 
  class toDo {
  protected    $identify;
@@ -29,13 +28,11 @@
      $this->startDate=$strtdte;
  }
  }
-  
- $ray=[];
-  
+    $ray = isset($_SESSION['ray']) ? $_SESSION['ray']: [];
+    $ide = isset($_SESSION['ide']) ? $_SESSION['ide'] : 1;
  if(isset($GET["unformal"],$_GET["date"],$_GET["submit"])){
      $unformal = htmlspecialchars($_GET["unformal"]);
      $date = htmlspecialchars($_GET["date"]);
-     $ide = isset($_SESSION['ide']) ? $_SESSION['ide'] : 1;
      array_push($ray, new toDo($ide,$unformal,$date));
      $ide++;
      $_SESSION['ide'] = $ide;
@@ -50,7 +47,7 @@
      <title>Document</title>
  </head>
  <body>
-     <form action="">
+     <form action="post">
          <input type="text" nane="unformal" id="unformal">
          <input type="text" name="date" id="date">
          <input type="submit" name="butto" id="submit">
@@ -63,34 +60,7 @@
      ?>
      </ul>
      <?php
-   function division(int $x, int $y) {
-       if($y > 0) {
-           return intval($x/$y);
-       }
-       else {
-           return "You cannot divide with 0.";
-       }
-   }
-   echo division("8.7", 19);
-   $num = "5.6";
-   echo "<br>";
-   echo 7/intval($num) . "<br>";
-   echo $num;
-?>
-<?php
-function upper(?string $str): string
-{
-    return "Returning " . strtoupper($str);
-}
-
-$str = "moikka";
-echo upper($str);
-?>
-<?php
-function add($x, $y): int {
-    return strval($x)+(string)$y;
-}
-echo "<br>" . add("3.5", "4.8");
-?>
+        echo isset($_SESSION['ray']);
+     ?>
  </body>
  </html> 
