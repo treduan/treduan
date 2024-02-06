61a37737a9e6975a830256af00dbb7840ff64bd4
Anna Metsäpelto <anna.metsapelto1@tampere.fi>
2024-02-06 13:00:27 +0200

    test

---
+++ php/testikansio/password.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// lomakkeenkäsittely
+// apumuuttuja - ollaanko kirjauduttu
+$ok = false; // oletuksena ei kirjauduttu
+// -- TÄHÄN LOMAKKEENKÄSITTELIJÄ --
+ 
+if (isset($_COOKIE["ok"])) {
+ $ok = $_COOKIE["ok"];
+ }
+ else if (isset($_POST["password"])) {
+ if ($_POST["password"] == "salasana") {
+ // käyttäjä antoi oikean salasanan
+ setcookie("ok", true, time() +1800);
+ echo "Tunnus oikein!";
+ $ok = true;
+ }
+ else {
+ // käyttäjä antoi väärän salasanan
+ echo "Tunnus väärin :( ";
+ }
+ }
+  // jos painettiin logoff-submit:
+if (isset($_POST["logoff"])) {
+    setcookie("ok","", time() -1800);
+    $ok = false;
+  }
+// 1. jos ei olla kirjauduttu
+if ($ok == false) {
+?>
+
+<form method="post">
+<label for="password">Syötä salasana</label><br />
+<input type="password" name="password" /><br />
+<input type="submit" value="Kirjaudu" />
+</form>
+<?php
+}
+else {
+   // 2. jos ollaan kirjauduttu niin
+?>
+
+<form method="post">
+<input type="submit" name="logoff" value="Kirjaudu ulos" />
+</form>
+<?php
+}
+?>
+
+
+</body>
+</html>
