a89243f0d688df5031abf5481ebe1ae28aadb5cf
Anna Metsäpelto <anna.metsapelto1@tampere.fi>
2026-01-26 07:59:47 +0200

    switch file name

--- selainohjelmointi2026/funktiot/functiontest.js
+++ selainohjelmointi2026/funktiot/functiontest.js
@@ -1,12 +1,4 @@
-const sum = (x, y) => {
-    return x + y;
-}
-
-console.log(sum(7, 9466.6));
-
-let info = `Name: Anna
-City: Tampere`;
-
-console.log(info);
-let number = Math.floor(Math.random() * 10 + 1);
-console.log(number);
+//define variable
+let name = "Anna";
+name = "Moi";
+console.log(name); //log name
